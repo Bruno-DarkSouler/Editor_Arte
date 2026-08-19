5fa04b82c006ebe00929f49a5abfa190a1de6c6d
Bruno-DarkSouler <brunofornasar@gmail.com>
2026-08-19 18:31:18 -0300

    Instalacion de express

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,39 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+ <div>
+ <h1>Lista de Tareas Colaborativa</h1>
+ <input v-model="newTask" @keyup.enter="addNewTask" placeholder="Nueva
+tarea" />
+ <ul>
+ <li v-for="task in todoStore.tasks" :key="task.id">
+  <input type="checkbox" :checked="task.done"
+@change="todoStore.toggleTask(task.id)" />
+ {{ task.title }}
+ </li>
+ </ul>
+ <p>Pendientes: {{ todoStore.pendingTasks.length }}</p>
+ <p>Completadas: {{ todoStore.doneTasks.length }}</p>
+ </div>
 </template>
-
-<style scoped></style>
+<script setup>
+import { ref } from 'vue'
+import { useTodoStore } from './stores/todo'
+const todoStore = useTodoStore()
+const newTask = ref('')
+function addNewTask() {
+ if (newTask.value.trim()) {
+ todoStore.addTask(newTask.value.trim())
+ newTask.value = ''
+ }
+}
+// Simular recepción de tarea remota cada 5 segundos
+import { onMounted } from 'vue'
+onMounted(() => {
+ setInterval(() => {
+ todoStore.receiveRemoteTask({
+ id: Date.now(),
+ title: `Tarea remota ${Math.floor(Math.random()*100)}`,
+ done: false
+ })
+ }, 5000)
+})
+</script>
